462e14e968aa0e761b9b0077e4d46ddca09d99c7
diegoleiras2 <diegoleiras@gmail.com>
2021-11-02 08:35:45 -0300

    Lista de patologias

---
+++ clientes.py
@@ -0,0 +1,29 @@
+cliente= {"Nome_completo: " : "Vazio da Silva",
+"Telefone: " : 0, 
+"Data_Nasc: " : 0,
+"Sexo: " : "M",
+"Nacionalidade: " : "",
+"Naturalidade: " : " ",
+"Endereço: ": "",
+"Bairro: ": "",
+"Cidade: " : "",
+"Estado: " : "",
+"Telefone_residencial:" : 0,
+"Telefone celular: " : 0,
+"Estado_civil:" : 0,
+"E-mail:" : 0, 
+"Nome_Mãe: " : "",
+"Idade_Mãe:" : 0,
+"Religião_mãe:" : "",
+"Nome_Pai:" : "",
+"Idade_pai: " : 0,
+"Religião_pai": "",
+"Nome_cônjuge:" : "",
+"Irmãos?" : "",
+"Nome_Irmão:" : "",
+"Idade_Irmão" : "",
+
+
+
+}
+print(cliente)        
---
+++ psicopatologias.py
@@ -0,0 +1,17 @@
+sensacao = ["Hiperestesia", "Hipoestesia", "Anestesia", "Analgesia"]
+percepcao = ["Agnosias",["agnosia Visual", "Agnosia Tátil","Agnosia Primária ou Perseptiva", "Agnosia Semântica", "Agnosia Auditiva", ], "Ilusão"]
+representacao = ["Alucinação",["Alucinação Auditiva", "Alucinação Visual", "Alucinação Autoscópia", "Alucinação Extracampinas",
+ "Alucinação Alfativas e Gustativa", "Alucinação Tátil", "Aluinação Cenestésicas",
+  "Alucinação do Sentido de Equilíbrio", "Alucinação Psíquica"],"Alucinose","Alucinose Penduncular","Pseudo-Alucinação","Visões Fantástias"]
+conceitos = ["Esquizofrenia", "Deficiência Mental", "Estados Demenciais", "Psicoses Sintomáticas"]
+juizo = ["Delirios", ["Percepção Delirante", "Ocorrência Delirante", "Reação Deliróide"] ,"Delirio de perseguição",
+ "Delirio de Relação", "Delirio de Influência", "Delirio de Ciúme", "Delirio de Grandeza",["Delirio Ambicioso", "Delirio de Invenção", "Delirio de Reforma", "Delirio erótico"] ]
+raciocinio = ["Inibição do pensamento", "Fuga das ideias", "Perseveração", "Prolixidade", "Incoerência", "Interceptação do Pensamento", "Complusãp a Pensar",
+"pensamento Obsessivo", "Psicose Maníaco - Depressiva", "Epilepsia", "Esquizofrenia", "Deficiência Mental", "Psicoses Orgânicas", "Psicodes Sintomáticas"]
+memoria = ["Hipermneisa", "Amnesia", ["Amnesia Anterógrada", "Amnesia Retrógrada", "Amnesia retroanterógrada ou Total", "Amnesia transitória"]]
+print(percepcao)
+print(sensacao)
+print(representacao)
+print(conceitos)
+print(juizo)
+print(raciocinio)
