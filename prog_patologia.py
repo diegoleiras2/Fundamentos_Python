7230efbc4c6a930dd968db5632d01ed55c911d64
diegoleiras2 <diegoleiras@gmail.com>
2021-11-09 09:11:27 -0300

    Inclusão do While

--- prog_patologia.py
+++ prog_patologia.py
@@ -10,32 +10,40 @@ raciocinio = ["Inibição do pensamento", "Fuga das ideias", "Perseveração", "
 "pensamento Obsessivo", "Psicose Maníaco - Depressiva", "Epilepsia", "Esquizofrenia", "Deficiência Mental", "Psicoses Orgânicas", "Psicodes Sintomáticas"]
 memoria = ["Hipermneisa", "Amnesia", ["Amnesia Anterógrada", "Amnesia Retrógrada", "Amnesia retroanterógrada ou Total", "Amnesia transitória"]]
 
-print("Categoria de Patoligias:\n 1- Sensação,\n 2-Percepção,\n 3- Representação,\n 4- Conceitos,\n 5-Raciocínio, \n 6- Memória. ")
 
-escolha = int(input("Escolha uma categoria: "))
+while True:
+  categoria = input("Deseja escolher uma categoria [s/n]? ")
+  
+  if categoria == "sim" or categoria == "s":
+    print("\nCategoria de Patoligias:\n 1- Sensação,\n 2- Percepção,\n 3- Representação,\n 4- Conceitos,\n 5- Raciocínio, \n 6- Memória.\n")
 
-if escolha == 1:
-  for tag in sensacao:
-    print(f"Psicopatologia da Sensação - {tag}")
+    escolha = int(input("\nEscolha uma categoria: "))
 
-elif escolha == 2:
-  for tag in percepcao:
-   print(f"Psicopatologia da Percepção - {tag}")
+    if escolha == 1:
+      for tag in sensacao:
+        print(f"Psicopatologia da Sensação - {tag}")
 
-elif escolha == 3:
-  for tag in representacao:
-   print(f"Psicopatologia de Representação - {tag}")
+    elif escolha == 2:
+      for tag in percepcao:
+        print(f"Psicopatologia da Percepção - {tag}")
 
-elif escolha == 4:
-  for tag in conceitos:
-   print(f"Psicopatologia de Conceito - {tag}")
+    elif escolha == 3:
+      for tag in representacao:
+        print(f"Psicopatologia de Representação - {tag}")
 
-elif escolha == 5:
-  for tag in juizo:
-    print(f"Psicopatologia de Juízo - {tag}")
+    elif escolha == 4:
+      for tag in conceitos:
+        print(f"Psicopatologia de Conceito - {tag}")
 
-else escolha == 6:
-  for tag in memoria:
-    print(f"Psicopatologia da Memória - {tag}")
- 
+    elif escolha == 5:
+      for tag in juizo:
+        print(f"Psicopatologia de Juízo - {tag}")
 
+    elif escolha == 6:
+      for tag in memoria:
+        print(f"Psicopatologia da Memória - {tag}")
+    
+    else:
+      print("Escolha uma categoria válida")
+  elif categoria == "não" or categoria == "n":
+    break
